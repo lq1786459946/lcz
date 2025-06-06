ae7ecf224994d8fb698d420385e82d5869c2d864
1786459946@qq.com <1786459946@qq.com>
2025-06-06 15:14:50 +0800

    🐞 fix:three.js

--- navigation/vertical/index.js
+++
@@ -1,6 +0,0 @@
-import analysisTools from "./analysis-tools.js";
-import personalCenter from "./personal-center.js";
-
-export const isLoginRoutes = [...analysisTools, ...personalCenter];
-
-export const routes = [...analysisTools];
--- pages/analysisTools/crm.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>分析工具</div>
-</template>
-
-<script setup></script>
